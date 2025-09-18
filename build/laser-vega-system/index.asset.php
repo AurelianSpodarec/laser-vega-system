fd7f3882684ba6782e5844a7182cc1d4f15ce532
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-09-18 23:42:43 +0100

    Initial commit

---
+++ build/laser-vega-system/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '15d7d34413a54a30b938');
---
+++ build/laser-vega-system/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '2033bddf601940f5557c');
