d1e594804485988830b5dedff2bc4442cbb842af
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-09-19 16:01:21 +0100

    chore: update category

--- build/blocks-manifest.php
+++ build/blocks-manifest.php
@@ -7,7 +7,7 @@ return array(
 		'name' => 'create-block/fiftyfifty-img-txt',
 		'version' => '0.1.0',
 		'title' => '50/50 Image Text',
-		'category' => 'widgets',
+		'category' => 'design',
 		'icon' => 'smiley',
 		'description' => 'A custom block that displays content in a 50/50 split layout with an image on one side and text on the other.',
 		'example' => array(
@@ -28,7 +28,7 @@ return array(
 		'name' => 'create-block/testimonial',
 		'version' => '0.1.0',
 		'title' => 'Single Testimonial',
-		'category' => 'text',
+		'category' => 'design',
 		'icon' => 'smiley',
 		'description' => 'Display a full width, single customer testimonial with text and author details.',
 		'example' => array(
